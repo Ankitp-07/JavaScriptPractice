24b5a28372cc16bc44f17985269ab19e51dc42f7
Ankitp-07 <152526088+Ankitp-07@users.noreply.github.com>
2026-06-28 17:08:10 +0530

    comparison & operations operators

---
+++ 01Basics for js/1.5camparison.js
@@ -0,0 +1,18 @@
+//console.log(2>1)
+//console.log("2">1)  //automatically convert it in number
+
+//console.log(null>0) -> false
+//console.log(null==0)   ->>false
+//console.log(null>=0) //->>> true (comparison converts null in number 0 but equality i.e == operates differently)
+ 
+
+
+//for undefined it gives false 
+
+//console.log(undefined==0)
+ //console.log(undefined>0)
+ //console.log(undefined<=0)    
+
+
+ //=== 
+  console.log("2"===2)  //->> false =>> strict checkk , checks datatype toooo
