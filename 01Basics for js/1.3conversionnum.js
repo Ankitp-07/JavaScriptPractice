00041fbdfbf3d3def20ddc8590ab30c6669ea1bf
Ankitp-07 <152526088+Ankitp-07@users.noreply.github.com>
2026-06-28 17:05:46 +0530

    conversion examples

---
+++ 01Basics for js/1.3conversionnum.js
@@ -0,0 +1,38 @@
+// let score="33vd"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueinnum= Number(score)
+// console.log(typeof valueinnum)
+// console.log(valueinnum)
+// let test =1
+// let isinboolean=Boolean(test)
+// console.log(isinboolean)
+
+
+
+// 1-> true
+//0-> false
+
+
+// let test ="ankit"
+// let isinboolean=Boolean(test)
+// console.log(isinboolean)
+
+//"ankit"-> true
+//""->false
+
+
+// let test =""
+// let isinboolean=Boolean(test)
+// console.log(isinboolean)
+ 
+
+
+// let a=23
+// let instring=String(a);
+// console.log(typeof instring);
+
+
+//******************** Operations ***************************************
+
