b9b01875fd6fdae3f2fa2aa62fcea5b2a0266a11
Ankitp-07 <152526088+Ankitp-07@users.noreply.github.com>
2026-06-28 17:08:37 +0530

    Added datatype summaries

---
+++ 01Basics for js/1.6datatype-summary.js
@@ -0,0 +1,38 @@
+let useremail
+useremail=Symbol("ankitnavodayan")
+
+let useremail1=Symbol("ankitnavodayan")
+console.log(useremail===useremail1)    //=>>> gives false due to use symbol which always give unique eithe thw inout is same 
+
+
+ //refernce ( array,object,functions)
+  
+
+//ARRAY 
+
+
+ const heros =["ankit","aditya" , 54]
+ console.log(heros)
+
+ 
+// Object
+ let myobj ={ name1:"ankit",
+    age :20
+ }
+
+
+ // functions
+ const myfunc=function(){
+    console.log("hello world")
+
+ }
+
+
+
+bignumber =234564532134553421343
+ console.log(typeof bignumber)
+
+ console.log(typeof myfunc)
+ console.log(typeof myobj)
+ console.log(typeof heros)
+  
