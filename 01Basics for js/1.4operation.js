3c5f1912d1fd8349bc7657411aa0749c052e3dd6
Ankitp-07 <152526088+Ankitp-07@users.noreply.github.com>
2026-06-28 17:07:01 +0530

    operations

---
+++ 01Basics for js/1.4operation.js
@@ -0,0 +1,22 @@
+let value=3
+let negvalue = -value  //negates the value 
+console.log(negvalue)
+
+
+str="ankit"
+str1="patel"
+console.log(str+str1)
+
+console.log("2"+2) //  ->22
+console.log("2"+1+2)  // -> 212  => jaha string aa gya waha se sb string treat hoga 
+console.log(2+1+"2")  // -> 32 => pahle operations ho gaye hain beacuse string baad me aaya hai
+console.log(2+"3"+4)  // -> 234
+
+console.log(+true);
+
+a=3
+++a
+console.log(a) //-> 4
+a++              //->  5
+console.log(++a) // -> 6 
+console.log(a++ +  a++ + ++a )  
