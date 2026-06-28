0122094d4ccf0ac5d13cbe37969f1248e4628088
Ankitp-07 <152526088+Ankitp-07@users.noreply.github.com>
2026-06-28 17:03:22 +0530

    Added datatype and conversion examples

---
+++ 01Basics for js/1.2datatype.js
@@ -0,0 +1,24 @@
+// "use strict";
+console.log(3+3)
+let name1="ankit"
+let age = 23
+let ishere=true
+console.table([name1,age,ishere])
+
+
+
+// Data types are:
+// number -> 2 to power 53
+//bigint 
+//string -> ""
+//boolean - true/false
+//null -> standalone value
+//undefined 
+//symbol
+//object
+
+let cat=null;
+console.log(typeof null); // object
+
+console.log(typeof undefined);
+console.log(typeof ishere)
