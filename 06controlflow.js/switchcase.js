0d0b93fad83f7384006ecf1440ddf656b671a885
Ankitp-07 <152526088+Ankitp-07@users.noreply.github.com>
2026-07-01 14:06:02 +0530

    switchcase

---
+++ 06controlflow.js/switchcase.js
@@ -0,0 +1,70 @@
+//switch(key){
+// case value:
+//     ...........
+//     .........
+//     break
+
+// ...
+// ...
+// ...
+// default:
+//     break
+// }
+
+const month=3
+switch(month)
+{
+        case 1:
+            console.log("Jan")
+            break
+        case 2:
+            console.log("feb")
+            break
+        case 3:
+            console.log("March")
+            break
+        case 4:
+            console.log("April")
+            break
+        case 5:
+            console.log("May")
+            break
+        case 6:
+            console.log("Jun")
+            break
+
+        default:
+            console.log("default case match")
+            break
+    }
+
+//string as case
+const month1="jan"
+switch(month1)
+{
+        case "jan":
+            console.log("Jan")
+            break
+        case "feb":
+            console.log("feb")
+            break
+        case "mar":
+            console.log("March")
+            break
+        case "april":
+            console.log("April")
+            break
+        case "may":
+            console.log("May")
+            break
+        case "jun":
+            console.log("Jun")
+            break
+
+        default:
+            console.log("default case match")
+            break
+    }
+
+
+    
